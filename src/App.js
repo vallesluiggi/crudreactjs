f2464ec69d723da29754f4b58f87c1eedeec9067
vallesluiggi <vallesluiggi@gmail.com>
2021-08-23 16:38:15 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+function App() {
+  return (
+    <div ClassName='container mt5'>
+     <h1>Hola con React</h1>
+     <hr/>
+    </div>
+  );
+}
+
+export default App;
